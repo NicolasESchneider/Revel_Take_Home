7b1db1bc7b05f0a057a3c924ef5229175c737015
Nicolas Schneider <nicolas.e.schneider@vanderbilt.edu>
2020-09-10 11:22:55 -0400

    revel takehome -final

--- flask/db.py
+++ flask/db.py
@@ -1,7 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
-SQLALCHEMY_DATABASE_URI = 'mysql://rev_admin:rev2020@localhost/revel_takehome'
+database_name = 'revel_takehome'
+database_user = 'rev_admin'
+database_password = 'rev2020'
 
+SQLALCHEMY_DATABASE_URI = 'mysql://rev_admin:rev2020@localhost/revel_takehome'
 db = SQLAlchemy()
 ma = Marshmallow()
